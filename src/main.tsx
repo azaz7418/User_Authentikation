b3b84da5321f0cea0b9d6b39efaa7738e535381b
azaz7418 <wazaz7418@gmail.com>
2025-12-07 17:56:16 +0600

    add some

--- src/main.tsx
+++ src/main.tsx
@@ -7,36 +7,36 @@ import { persistor, store } from "./redux/store.ts";
 import { PersistGate } from "redux-persist/integration/react";
 import { QueryClient, QueryClientProvider } from "react-query";
 // import axios from "axios";
-import { ConfigProvider, theme } from "antd";
+// import { ConfigProvider,  } from "antd";
 
 const queryClient = new QueryClient();
 
 // axios.defaults.baseURL = "http://localhost:5000/api/v1";
 
-const themeConfig = {
-  token: {
-    colorPrimary: "#ff6347", // Green primary color
-    colorText: "#ff6347",
-    colorIcon: "#1DA57A",
-    // colorTextButtonHover: "#52c41a",
-    hoverColor: "#fffff",
-  },
+// const themeConfig = {
+//   token: {
+//     colorPrimary: "#ff6347", // Green primary color
+//     colorText: "#ff6347",
+//     colorIcon: "#1DA57A",
+//     // colorTextButtonHover: "#52c41a",
+//     hoverColor: "#fffff",
+//   },
 
-  // 1. Use dark algorithm
-  algorithm: theme.darkAlgorithm,
+//   // 1. Use dark algorithm
+//   algorithm: theme.darkAlgorithm,
 
-  // 2. Combine dark algorithm and compact algorithm
-  // algorithm: [theme.darkAlgorithm, theme.compactAlgorithm],
-};
+//   // 2. Combine dark algorithm and compact algorithm
+//   // algorithm: [theme.darkAlgorithm, theme.compactAlgorithm],
+// };
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <QueryClientProvider client={queryClient}>
-          <ConfigProvider theme={themeConfig}>
+          {/* <ConfigProvider theme={themeConfig}>
+          </ConfigProvider> */}
             <App />
-          </ConfigProvider>
         </QueryClientProvider>
       </PersistGate>
     </Provider>
